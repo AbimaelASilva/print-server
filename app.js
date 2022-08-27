d81fc430b1f5cc879303b7ddbb7f8eddb92d6414
Abimael <abimael.rza@gmail.com>
2022-08-27 11:44:40 -0300

    funcionando impressão

--- app.js
+++ app.js
@@ -3,54 +3,38 @@
 //https://medium.com/@renidelonzek/gerando-pdfs-com-flutter-64fc05eb7088
 
 import express from "express";
-//import { createRequire } from "module";
-//const require = createRequire(import.meta.url);
-
 
 const app = express();
 
-
 import pkg from 'pdf-to-printer';
+
 const { print } = pkg;
-import { create } from 'pdf-creator-node';
-import { readFileSync } from 'fs';
-import { writeFileSync } from 'fs';
-//const html = readFileSync('public/template.html', 'utf-8');
-import path, { extname } from "path";
 
+
+//AS DUAS LINHAS À BAIXO SERVEM PARA CONFIGURAR NA ROTA A RECEPÇÃO DO ARQUIVO
 import multer from 'multer';
-
-
-// var storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, 'uploads/')
-//     },
-//     filename: function (req, file, cb) {
-//         cb(null, Date.now() + path.extname(file.originalname)) //Appending extension
-//     }
-// });
-
+//AQUI EM "orderToPrint" estou definindo para salvar o arquivo na pasta "orderToPrint" que está na raiz do projeto
 const upload = multer({ dest: 'orderToPrint', });
 
-
-
-
+//O valor "order"  que está informado em "upload.single('order')"  deve ser o mesmo nome cha chave lá no envio no app:
+//Ex:
+/*-Parte do código lá no app
+  var formData = FormData.fromMap({
+        'orderNumber': '#2022',
+        'order': await MultipartFile.fromFile(
+          file.path,
+          filename: 'order',
+        )
+      });
+*/
 app.post('/sendPrint', upload.single('order'), (request, response) => {
 
-    console.log('CHEGOU AQUI!!');
-    console.log(request.file);
-    console.log(request.body);
+
     const filename = request.file.filename;
-    //console.log(filename);
-    console.log(`C:/orderToPrint/${filename}.pdf`);
+    //Aqui estou montando o caminho onde está o arquivo(foi salvo) o arquivo que é recebido pelo EP
     const pathPdf = `C:orderToPrint/${filename}`;
 
-
-    //const tmpFilePath = path.join('./tmp/${Math.random().toString(36).substr(7)}.pdf');
-
-    //   writeFileSync(tmpFilePath, request.body, 'binary');
-
-
+    //Aqui de fato estamos enviando o arquivo para a impressão
     print(pathPdf).then(console.log);
 
     return response.json({
@@ -59,60 +43,3 @@ app.post('/sendPrint', upload.single('order'), (request, response) => {
 });
 
 app.listen(4003, () => console.log('Servidor está rodando na porta 4003'));
-
-
-
-
-/*
-  const options = {
-        format: "A4",
-        orientation: "portrait",
-        border: "10mm",
-        header: {
-            height: "45mm",
-            contents: '<div style="text-align: center;">Author: Abimael Andrade</div>'
-        },
-        footer: {
-            height: "28mm",
-            contents: {
-                first: 'Cover page',
-                2: 'Second page', // Any page number is working. 1-based index
-                default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
-                last: 'Last Page'
-            }
-        }
-    };
-
-    const users = [
-        {
-            name: 'Rozana',
-            age: 21
-        },
-        {
-            name: 'Suzana',
-            age: 10
-        },
-        {
-            name: 'Gato Miu',
-            age: 2
-        },
-    ];
-
-    const document = {
-        html: html,
-        data: {
-            users: users
-        },
-        path: './listaDeUsuarios.pdf',
-        type: '',
-    }
-
-    create(document, options).then((response) => {
-
-        console.log(response);
-
-        print("./listaDeUsuarios.pdf").then(console.log);
-        
-        
-    }).catch((error) => console.log(error));
-*/
